8c42bd01df35afee956610289db79cde98e13416
1991528871-eng <1991528871@g.ecc.u-tokyo.ac.jp>
2026-06-25 19:15:49 +0900

    Create load_input_data.py

---
+++ src/my_library/load_input_data.py
@@ -0,0 +1,12 @@
+def load(path):
+    sentence_arrays = []
+    with open(path, "r", encoding = "utf-8") as f:
+        lines = f.read()
+        for l in lines.split("\n"):
+            if l != "":
+                sentence_arrays.append([ word_set.split(",") for word_set in l.split(" ") ])
+    return sentence_arrays
+
+path = "data/test_data.txt"
+sentence_arrays = load("data/test_data.txt")
+print(sentence_arrays)
