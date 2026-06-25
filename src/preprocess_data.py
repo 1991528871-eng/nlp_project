f00736104acf080ba2f7ed0ba4f52bb95a2e79b7
1991528871-eng <1991528871@g.ecc.u-tokyo.ac.jp>
2026-06-25 19:15:11 +0900

    Add files via upload

---
+++ src/preprocess_data.py
@@ -0,0 +1,20 @@
+from janome.tokenizer import Tokenizer
+import chardet
+
+def into_str(str_list):
+    output = str_list[0][0] + "," + str_list[0][1]
+    for i in range(1, len(str_list)):
+        output = output + " " + str_list[i][0] + "," + str_list[i][1]
+    return output
+
+with open("data/data.txt", "r", encoding = "utf-8") as f:
+    lines = f.read()
+    t = Tokenizer()
+    sentences = []
+    for l in lines.split("\n"):
+        # print(list(t.tokenize(l, wakati = True)))
+        sentences.append([ [token.surface, token.base_form] for token in t.tokenize(l) ])
+
+with open("data/processed_data.txt", "w", encoding = "utf-8") as f:
+    for tokens in sentences:
+        print(into_str(tokens), file = f)
